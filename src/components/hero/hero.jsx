b4faf17f2d437adbe885e08c454663bd3fb61ee5
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-14 15:27:41 -0600

    Update hero.jsx

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,10 +1,9 @@
 import "./hero.css";
-import LandingIMG from "../../../public/landing.jpg"
 
 export default function Hero() {
   return (
     <div className="HeroContainer">
-      <img src={LandingIMG} alt="Hero Banner" />
+      <h1>Hero Banner</h1>
     </div>
   );
 }

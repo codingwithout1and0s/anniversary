6e6bc1c011fee5da4d7c31462b126ee5efe9ab28
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-17 14:45:17 -0600

    Update hero.jsx

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,9 +1,11 @@
 import "./hero.css";
-
 export default function Hero() {
   return (
-    <div className="HeroContainer">
-      <h1>Hero Banner</h1>
+    <div className="heroContainer">
+      <div className="heroCaption">
+        <h1>Steve & Michelle</h1>
+        <h2>Love @ First Sight</h2>{" "}
+      </div>
     </div>
   );
 }

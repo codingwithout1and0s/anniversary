98bd52bb4d814e56c8cddf0cb33fb9dac82103bc
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-17 15:29:07 -0600

    Create dataSlider.jsx

---
+++ src/components/slider/dataSlider.jsx
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum1",
+    subTitle: "Lorem1"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum2",
+    subTitle: "Lorem2"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum3",
+    subTitle: "Lorem3"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum4",
+    subTitle: "Lorem4"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum5",
+    subTitle: "Lorem5"
+  }
+];
+
+export default dataSlider;

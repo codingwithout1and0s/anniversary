29f0ae746a55857036dc52de7ce44c5852133fd8
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-17 14:48:00 -0600

    Update testimonials.jsx

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -4,21 +4,24 @@ export default function Testimonials() {
   const dummyData = [
     {
       id: "1",
-      profileIMG: "test.png",
+      profileIMG:
+        "https://tse3.mm.bing.net/th/id/OIP.qWdGX4zl1J9BkoHaRRL95AAAAA?pid=ImgDet&rs=1",
       person: "Tom Cruise",
       testimonial:
-        "Those two are were dreams come true. I am so jealous. They are the reason I'm still looking for love."
+        "Those two are a dream come true. I am so jealous. They're the reason I'm still looking for love."
     },
     {
       id: "2",
-      profileIMG: "test1.png",
-      person: "Johnny Galecki",
+      profileIMG:
+        "https://tse1.mm.bing.net/th/id/OIP.BINR8Y51OEAMYqvWQVecBgAAAA?pid=ImgDet&rs=1",
+      person: "Ed Sheeran",
       testimonial:
-        "Gosh, she looks badass in that elongated white wedding dress with the classic vintage leather jacket. Yeah and the dude looks nice too."
+        "Gosh, she looks badass in that elongated white wedding dress with the classic vintage leather jacket. Yeah and the dude looks nice too..."
     },
     {
       id: "3",
-      profileIMG: "test2.png",
+      profileIMG:
+        "https://th.bing.com/th/id/R.68d55aa4711164caeaf6a7e6719b87ae?rik=%2bWTh1%2bwnZXecpQ&pid=ImgRaw&r=0",
       person: "Rob Lowe",
       testimonial:
         "I was paid to leave a testimonial. Go team um... Steve-Michelle?"
@@ -28,21 +31,22 @@ export default function Testimonials() {
   return (
     <div className="testimonials">
       <h1>Testimonials</h1>
-      {dummyData.map( card => (
-              <div className="card"  key={card.id} >
-              <div className="layer"></div>
-              <div className="content">
-                  <p>
-                      {card.testimonial}
-                      <div className="details">
-                          <h2><span>{card.person}</span></h2>
-                      </div>
-                  </p>
+      {dummyData.map((card) => (
+        <div className="card" key={card.id}>
+          <div className="layer"></div>
+          <div className="content">
+            <img className="image" src={card.profileIMG} alt={card.person} />
+            <p>
+              {card.testimonial}
+              <div className="details">
+                <h2>
+                  <span>{card.person}</span>
+                </h2>
               </div>
-              </div>
+            </p>
+          </div>
+        </div>
       ))}
     </div>
   );
-
-
 }

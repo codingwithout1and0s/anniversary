02471f53c7fae71f646c7b216b8cf2778a4153b0
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-17 14:47:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./styles.css";
-import MainContent from "./components/maincontent/maincontent";
+
+import Header from "./components/header/header";
+import Compliment from "./components/compliment/compliment";
+import Signature from "./components/signature/signature";
+import Testimonials from "./components/testimonials/testimonials";
+import Closer from "./components/closer/closer";
+import Footer from "./components/footer/footer";
+import Card from "./components/card/card";
+import Slider from "./components/slider/slider";
+import Hero from "./components/hero/hero";
 
 export default function App() {
   return (
     <div className="App">
-      <MainContent />
+      <Header />
+      <Hero />
+      <Card id={0} />
+      <Compliment />
+      <Card id={1} />
+      <Signature />
+      <Testimonials />
+      <Slider />
+      <Closer />
+      <Footer />
     </div>
   );
 }

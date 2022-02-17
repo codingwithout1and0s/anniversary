50228f89930e23dca42b60aab15a6ac167247f68
codingwithout1and0s <69809025+codingwithout1and0s@users.noreply.github.com>
2022-02-17 15:28:55 -0600

    Create btnSlider.jsx

---
+++ src/components/slider/btnSlider.jsx
@@ -0,0 +1,14 @@
+import "./slider.css";
+import leftArrow from "./icons/left-arrow.svg";
+import rightArrow from "./icons/right-arrow.svg";
+
+export default function BtnSlider({ direction, moveSlide }) {
+  return (
+    <button
+      onClick={moveSlide}
+      className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+    >
+      <img src={direction === "next" ? rightArrow : leftArrow} />
+    </button>
+  );
+}

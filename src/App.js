1e1977bb15d99793299765a203c9e8521609a56c
codingwithout1and0s <codingwithout1and0s@gmail.com>
2022-02-26 13:08:03 -0600

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./styles.css";
-<<<<<<< HEAD
 
 import Header from "./components/header/header";
 import Compliment from "./components/compliment/compliment";
@@ -10,14 +9,10 @@ import Footer from "./components/footer/footer";
 import Card from "./components/card/card";
 import Slider from "./components/slider/slider";
 import Hero from "./components/hero/hero";
-=======
-import MainContent from "./components/maincontent/maincontent";
->>>>>>> 4b6d864ce6bef9f42cc46a372f632b07bfdf0b70
 
 export default function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Header />
       <Hero />
       <Card id={0} />
@@ -28,9 +23,6 @@ export default function App() {
       <Slider />
       <Closer />
       <Footer />
-=======
-      <MainContent />
->>>>>>> 4b6d864ce6bef9f42cc46a372f632b07bfdf0b70
     </div>
   );
 }

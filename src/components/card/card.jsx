0aae9db6a2e4c65d1969c887b8e9b7333d42f84b
codingwithout1and0s <codingwithout1and0s@gmail.com>
2022-02-26 14:31:19 -0600

    Updated card.jsx and hero.jsx

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,18 +5,18 @@ export default function CardTest(props) {
     {
       id: "1",
       cardIMG:
-        "https://images.unsplash.com/photo-1570366404839-173099e1d372?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG5oYSUyMHRyYW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      cardTitle: "Meet Steve",
+        "https://images.unsplash.com/photo-1628496893434-36e7f807af78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      cardTitle: "Meet Thang",
       cardText:
-        "You have secured the perfect ideal man, enjoying his embrace, and comtemplating your eternity together."
+        "A simple happy go lucky kind of guy"
     },
     {
       id: "2",
       cardIMG:
         "https://images.unsplash.com/photo-1445272727255-681d14e89af1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
-      cardTitle: "Meet Michelle",
+      cardTitle: "Meet Ngoc",
       cardText:
-        "Look at you! You did it! You found the perfect/unperfect man just for you."
+        "Really the best girl looking for a simple lovely life with a good man"
     }
   ];
 
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
     <div className="heroContainer">
       <div className="heroCaption">
-        <h1>Steve & Michelle</h1>
+        <h1>Thang & Ngoc</h1>
         <h2>Love @ First Sight</h2>{" "}
       </div>
     </div>
